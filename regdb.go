ec6edea7eb71a9b3696d26090391bf83f6daf75c
AkarshSahlot <akarshsahlot7@gmail.com>
2026-03-06 23:30:32 +0530

    chore: add go-mfp attribution header to go files
    Added the original author, license, and 'Adapted from' block to the top of all our Go files to keep the licensing clear.

--- regdb.go
+++ regdb.go
@@ -4,6 +4,10 @@
 // Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
 // See LICENSE for license terms and conditions
 //
+// Adapted from github.com/OpenPrinting/go-mfp
+// Original author: Alexander Pevzner
+// License: BSD 2-Clause
+//
 // Registrations database
 
 package main
